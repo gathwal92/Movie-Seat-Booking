c18d32752ac4888f9a91e27e4a734ca688dc60c8
Gurpreet Athwal <gurpreetathwal@willowmarketing.com>
2020-06-28 13:20:25 -0400

    added ability to toggle seats

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const cound = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = +movieSelect.value;
+
+container.addEventListener('click', (e) => {
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+    }
+})
